9830b495cfe4c2ccba25d79000188bc596f1bee1
kamron <kbariev7@gmail.com>
2024-05-02 23:24:25 +0500

    starterTgBot

---
+++ src/main/java/org/example/echobot/EchoBot.java
@@ -0,0 +1,33 @@
+package org.example.echobot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class EchoBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "internProject2_bot"; // Замените на имя вашего бота
+    }
+
+    @Override
+    public String getBotToken() {
+        return "7009003780:AAFZr6CgMfb28CSTXaccjiA7lwhCeGTRXd8"; // Замените на токен вашего бота
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage();
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
